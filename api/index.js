d830a20aec18008f3ab6ed16a643b1dfd1c5114b
practice404 <hawkempire007@gmail.com>
2021-11-16 01:43:47 +0530

    working on backend api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,13 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryPost = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
+
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 dotenv.config();
 app.use(express.json());
@@ -12,7 +19,24 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "hello.png");
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
 app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryPost);
 
 app.listen("3001", () => {
   console.log("Backend is running.");
